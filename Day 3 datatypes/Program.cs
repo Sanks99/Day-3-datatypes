8572a42a819208716ff0221f08b5710c9036b0e3
Sankalpa <sankalpa.gaidhane@resolve.io>
2023-08-12 16:14:59 +0530

    Add project files.

---
+++ Day 3 datatypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Implicit Conversion
+        float floatValue = 3.14f;
+        double doubleValue = floatValue;
+        Console.WriteLine($"floatValue: {floatValue}, doubleValue: {doubleValue}");
+
+        // Explicit Conversion
+        double explicitDoubleValue = 123.456;
+        int intValue = (int)explicitDoubleValue;
+        Console.WriteLine($"explicitDoubleValue: {explicitDoubleValue}, intValue: {intValue}");
+
+        // Built-in Methods
+        string intAsString = intValue.ToString();
+        double parsedDouble = double.Parse("7.89");
+        Console.WriteLine($"intAsString: {intAsString}, parsedDouble: {parsedDouble}");
+
+       
+        string firstName = "John";
+        string lastName = "Doe";
+        string fullName = firstName + " " + lastName;
+        Console.WriteLine($"fullName: {fullName}");
+
+        string upperFullName = fullName.ToUpper();
+        Console.WriteLine($"upperFullName: {upperFullName}");
+
+
+    }
+}
